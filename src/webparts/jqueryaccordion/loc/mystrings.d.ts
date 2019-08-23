9faab08fb6dbfc91cba1d6f10d47edcfc911851e
prabhuspfx <54140444+prabhuspfx@users.noreply.github.com>
2019-08-23 16:26:36 +0530

    Initial commit

---
+++ src/webparts/jqueryaccordion/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IJqueryaccordionWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'JqueryaccordionWebPartStrings' {
+  const strings: IJqueryaccordionWebPartStrings;
+  export = strings;
+}
